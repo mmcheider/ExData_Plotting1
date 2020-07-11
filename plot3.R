6d50a8fe20aa986b68c6244e7e98b998e7af57cf
mmcheider <mmcheider@gmail.com>
2020-07-10 23:49:55 -0700

    Minor changes

--- plot3.R
+++ plot3.R
@@ -5,13 +5,13 @@
 #              consumption in one household with a one-minute sampling rate over
 #              a period of almost 4 years. Different electrical quantities and 
 #              some sub-metering values are available.
-
+#
 # load/read data into R
 mydata <- read.table("household_power_consumption.txt", header = TRUE, sep = ";"
                      ,stringsAsFactors = FALSE)
 # only use data from the dates 2007-02-01 and 2007-02-02
 mydata <- mydata[66638:69518, ] 
-# convert date and time character vectors to class "POSIXlt"
+# convert date and time character vectors to class of "POSIXlt"
 z <- strptime(paste(mydata$Date, mydata$Time), "%d/%m/%Y %H:%M:%S")
 # make the plot
 with(mydata, 
@@ -26,7 +26,8 @@ with(mydata,
                    lty = 1, lwd = 1)
              # add Legend
              legend("topright",legend = c("Sub_metering_1","Sub_metering_2",
-                                          "Sub_metering_3"), col=c("black","red","blue"), lty=1, xjust = 0)
+                                          "Sub_metering_3"), col=c("black","red"
+                                          ,"blue"), lty=1, xjust = 0)
      } 
 )
 # copy the plot to .png file device
